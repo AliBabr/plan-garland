b23267c73a337c39583185e0aa97c25b007d118e
hasalgorepublic <hamza.sohail@algorepublic.com>
2020-12-18 05:24:48 +0500

    Login Signup Dashboard

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    authenticated :user do
+      root "dashboard#index", as: :unauthenticated_root
+    end
+    unauthenticated do
+      root "devise/sessions#new", as: :authenticated_root
+    end
+  end
+end
---
+++ db/migrate/20201217232436_add_columns_to_user.rb
@@ -0,0 +1,11 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :username, :string
+    add_column :users, :company_name, :string
+    add_column :users, :country, :string
+    add_column :users, :address, :string
+    add_column :users, :phone_number, :string
+  end
+end
