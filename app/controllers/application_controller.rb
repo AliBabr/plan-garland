dd5ce500c8079122d24c731c9189c949e4a0589f
hasalgorepublic <hamza.sohail@algorepublic.com>
2020-12-21 15:51:46 +0500

    Logout Button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :company_name, :country, :address, :phone_number])
   end
-  
 end
